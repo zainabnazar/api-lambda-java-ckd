63fe7c35324b1584dc4458565ce3922fa975d924
zainabnoori <zainab.noori@and.digital>
2023-01-26 14:45:08 +0000

    Add the lambda, api and the inegration

--- bmi-calculator/src/main/java/com/tericcabrel/App.java
+++ bmi-calculator/src/main/java/com/tericcabrel/App.java
@@ -1,13 +1,41 @@
 package com.tericcabrel;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        logger.info(input.getBody());
+
+        Gson gson = new Gson();
+        RequestInput bodyInput = gson.fromJson(input.getBody(), RequestInput.class);
+
+        double result = calculateBodyMassIndex(bodyInput.getHeight(), bodyInput.getWeight());
+        String output = String.format("{ \"result\": %s }", result);
+
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Content-Type", "application/json");
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(responseHeaders);
+
+        return response
+                .withStatusCode(200)
+                .withBody(output);
+    }
+
+    private double calculateBodyMassIndex(double height, double weight) {
+        double bmi = weight / Math.pow(height, 2);
+        double bmiRounded = Math.round(bmi * 10);
+
+        return  bmiRounded / 10;
     }
 }
